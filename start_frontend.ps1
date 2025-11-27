fa489b7093f9ad3b17da8650fbff1caffbdb9aaf
CristianCamiloForero <cristianc_forero@soy.sena.edu.co>
2025-11-27 14:46:17 -0500

    Delete start_frontend.ps1

--- start_frontend.ps1
+++
@@ -1,8 +0,0 @@
-# Script para iniciar el frontend
-Write-Host "Iniciando servidor HTTP del frontend..." -ForegroundColor Green
-Write-Host "Frontend disponible en: http://127.0.0.1:8080" -ForegroundColor Cyan
-Write-Host "Presiona Ctrl+C para detener" -ForegroundColor Yellow
-Write-Host ""
-
-cd .\frontend
-python -m http.server 8080 --bind 127.0.0.1
